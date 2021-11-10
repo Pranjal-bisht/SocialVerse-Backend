637cbcafa243ef6d78bdd0f5caf6786188cccb9f
Pranjal Bisht <bishtpranjal27@gmail.com>
2021-11-10 10:01:51 +0530

    Added API's for follow , unfollow, display my followings

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ router.get('/protected',requireLogin,(req,res)=>{
     res.send("hello user")
 })
 router.post('/signup',(req,res)=>{
-  const {name,email,password} = req.body 
+  const {name,email,password,pic} = req.body 
   if(!email || !password || !name){
      return res.status(422).json({error:"please add all the fields"})
   }
@@ -38,6 +38,7 @@ router.post('/signup',(req,res)=>{
                 email,
                 password:hashedpassword,
                 name,
+                pic
             })
     
             user.save()
